6bc91ae6533c59ea5df79067cdb0b320baa45aa8
Diegouninter <113302@alumnouninter.mx>
2025-11-24 07:52:41 -0600

    Agregar archivos de proyecto.

---
+++ Clase/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Clase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label labelMoves;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelBoard;
+        private System.Windows.Forms.Timer timerGame;
+        private System.Windows.Forms.Timer revealTimer;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.labelMoves = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.tableLayoutPanelBoard = new System.Windows.Forms.TableLayoutPanel();
+            this.timerGame = new System.Windows.Forms.Timer(this.components);
+            this.revealTimer = new System.Windows.Forms.Timer(this.components);
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.buttonRestart);
+            this.panelTop.Controls.Add(this.labelTime);
+            this.panelTop.Controls.Add(this.labelMoves);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Height = 56;
+            this.panelTop.Padding = new System.Windows.Forms.Padding(8);
+            // 
+            // labelMoves
+            // 
+            this.labelMoves.AutoSize = true;
+            this.labelMoves.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelMoves.Location = new System.Drawing.Point(12, 18);
+            this.labelMoves.Name = "labelMoves";
+            this.labelMoves.Size = new System.Drawing.Size(128, 19);
+            this.labelMoves.Text = "Movimientos: 0";
+            // 
+            // labelTime
+            // 
+            this.labelTime.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelTime.Location = new System.Drawing.Point(200, 18);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(86, 19);
+            this.labelTime.Text = "Tiempo: 00:00";
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            this.buttonRestart.Location = new System.Drawing.Point(680, 12);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(96, 32);
+            this.buttonRestart.TabIndex = 2;
+            this.buttonRestart.Text = "Reiniciar";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // tableLayoutPanelBoard
+            // 
+            this.tableLayoutPanelBoard.ColumnCount = 6;
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowCount = 6;
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.666666F));
+            this.tableLayoutPanelBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelBoard.Location = new System.Drawing.Point(0, 56);
+            this.tableLayoutPanelBoard.Name = "tableLayoutPanelBoard";
+            this.tableLayoutPanelBoard.Padding = new System.Windows.Forms.Padding(8);
+            this.tableLayoutPanelBoard.Size = new System.Drawing.Size(800, 394);
+            this.tableLayoutPanelBoard.TabIndex = 1;
+            // 
+            // timerGame
+            // 
+            this.timerGame.Interval = 1000;
+            this.timerGame.Tick += new System.EventHandler(this.timerGame_Tick);
+            // 
+            // revealTimer
+            // 
+            this.revealTimer.Interval = 700;
+            this.revealTimer.Tick += new System.EventHandler(this.revealTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanelBoard);
+            this.Controls.Add(this.panelTop);
+            this.Name = "Form1";
+            this.Text = "Memorama 6x6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Clase/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Clase
+{
+    public partial class Form1 : Form
+    {
+        private const int GridSize = 6;
+        private Button[,] boardButtons;
+        private string[] values; // pares mezclados
+        private Button firstClicked;
+        private Button secondClicked;
+        private int moves;
+        private int matchesFound;
+        private int secondsElapsed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            // Reset estado
+            timerGame.Stop();
+            revealTimer.Stop();
+            secondsElapsed = 0;
+            moves = 0;
+            matchesFound = 0;
+            firstClicked = null;
+            secondClicked = null;
+            labelMoves.Text = "Movimientos: 0";
+            labelTime.Text = "Tiempo: 00:00";
+
+            // Preparar valores (18 pares)
+            var baseValues = new List<string>();
+            // Usamos letras A..R (18 símbolos)
+            for (char c = 'A'; c < 'A' + (GridSize * GridSize / 2); c++)
+                baseValues.Add(c.ToString());
+
+            values = baseValues.Concat(baseValues).ToArray(); // duplicar
+            Shuffle(values);
+
+            // Crear botones dinámicamente y añadir al TableLayoutPanel
+            tableLayoutPanelBoard.Controls.Clear();
+            tableLayoutPanelBoard.ColumnCount = GridSize;
+            tableLayoutPanelBoard.RowCount = GridSize;
+
+            boardButtons = new Button[GridSize, GridSize];
+
+            for (int r = 0; r < GridSize; r++)
+            {
+                for (int c = 0; c < GridSize; c++)
+                {
+                    var btn = new Button();
+                    btn.Dock = DockStyle.Fill;
+                    btn.Margin = new Padding(6);
+                    btn.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+                    btn.BackColor = Color.LightSlateGray;
+                    btn.ForeColor = btn.BackColor; // oculto
+                    btn.Tag = values[r * GridSize + c];
+                    btn.Text = ""; // oculto inicialmente
+                    btn.Click += CardButton_Click;
+                    boardButtons[r, c] = btn;
+                    tableLayoutPanelBoard.Controls.Add(btn, c, r);
+                }
+            }
+
+            // Iniciar timer del juego
+            timerGame.Start();
+        }
+
+        private void Shuffle<T>(T[] array)
+        {
+            var rng = new Random();
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                var tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+
+        private void CardButton_Click(object sender, EventArgs e)
+        {
+            if (revealTimer.Enabled)
+                return; // esperando para ocultar la pareja no coincidente
+
+            var clicked = sender as Button;
+            if (clicked == null)
+                return;
+
+            // ignorar si ya está descubierta
+            if (clicked.ForeColor == Color.Black)
+                return;
+
+            // revelar
+            clicked.Text = clicked.Tag.ToString();
+            clicked.ForeColor = Color.Black;
+
+            if (firstClicked == null)
+            {
+                firstClicked = clicked;
+                return;
+            }
+
+            if (clicked == firstClicked)
+                return;
+
+            secondClicked = clicked;
+            moves++;
+            labelMoves.Text = $"Movimientos: {moves}";
+
+            // comprobar coincidencia
+            if (firstClicked.Tag.ToString() == secondClicked.Tag.ToString())
+            {
+                // coincidencia: dejar visibles y deshabilitar
+                firstClicked.Enabled = false;
+                secondClicked.Enabled = false;
+                firstClicked = null;
+                secondClicked = null;
+                matchesFound += 1;
+                // Cada match incrementa en 1 (pero hay 18 pares)
+                if (matchesFound >= (GridSize * GridSize / 2))
+                {
+                    timerGame.Stop();
+                    MessageBox.Show($"¡Felicidades! Terminaste en {moves} movimientos y {FormatTime(secondsElapsed)}.", "Juego completo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                // no coincidencia: iniciar temporizador para ocultar
+                revealTimer.Start();
+            }
+        }
+
+        private void revealTimer_Tick(object sender, EventArgs e)
+        {
+            revealTimer.Stop();
+
+            if (firstClicked != null)
+            {
+                firstClicked.Text = "";
+                firstClicked.ForeColor = firstClicked.BackColor;
+            }
+            if (secondClicked != null)
+            {
+                secondClicked.Text = "";
+                secondClicked.ForeColor = secondClicked.BackColor;
+            }
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void timerGame_Tick(object sender, EventArgs e)
+        {
+            secondsElapsed++;
+            labelTime.Text = $"Tiempo: {FormatTime(secondsElapsed)}";
+        }
+
+        private string FormatTime(int totalSeconds)
+        {
+            int mins = totalSeconds / 60;
+            int secs = totalSeconds % 60;
+            return $"{mins:00}:{secs:00}";
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
